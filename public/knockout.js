9c8299b6afe7aa03ec6f5c83abdeaaebf3b81c1a
Loren Norman <lorennorman@gmail.com>
2010-08-28 16:49:03 -0400

    Added a scripts directory for executable scripts.
    Added a build script for concatenating the client source files together with Sprockets.
    
    Added the inital client files and proved everything worked with Jasmine tests.

---
+++ public/knockout.js
@@ -0,0 +1,3 @@
+var KNOCKOUT = NKO = {}
+
+KNOCKOUT.Constants = {};
---
+++ spec/client/KnockoutSpec.js
@@ -0,0 +1,12 @@
+describe("Setting up the KNOCKOUT project", function()
+{
+  it("should define a global constant", function()
+  {
+    expect(KNOCKOUT).toBeDefined();
+  })
+
+  it("should define a short-hand alias", function()
+  {
+    expect(NKO).toBeDefined();
+  })
+})
