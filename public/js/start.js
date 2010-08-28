9a74b65f7e247f3a503adbfe48f5acc6cd0825a7
Stafford Brooke <stafford@inquus.com>
2010-08-28 17:02:10 -0400

    themeing

---
+++ public/js/start.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.user-list').addClass('ui-widget-content');
+  $('.user-list ul').addClass('ui-widget-content');
+  $('.user-list h2').addClass('ui-widget-header');
+  $('.user-list li').addClass('ui-state-default');
+  $('#nav li').addClass('ui-state-default')
+  $('#nav li.active').addClass('ui-state-active')
+});
