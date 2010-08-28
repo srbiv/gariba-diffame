a9ac86ed88713014f3a11b69627102210f37c51b
Stafford Brooke <stafford@inquus.com>
2010-08-28 17:15:53 -0400

    button

--- public/js/start.js
+++ public/js/start.js
@@ -3,6 +3,5 @@ $(document).ready(function() {
   $('.user-list ul').addClass('ui-widget-content');
   $('.user-list h2').addClass('ui-widget-header');
   $('.user-list li').addClass('ui-state-default');
-  $('#nav li').addClass('ui-state-default')
-  $('#nav li.active').addClass('ui-state-active')
+
 });
