f1150510cc3f641c36d60c4d0824237f935af566
Loren Norman <lorennorman@gmail.com>
2010-08-28 13:47:58 -0400

    removed accidental npm cruft. added a config block to express, seems to be acting right now.

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-var app = require('express').createServer();
-app.register('.html', require('jade'));
-app.set('view engine', 'jade');
-app.get('/', function(req, res){
-    res.render('index.html');
+// requires
+var express = require('express');
+
+// definitions
+var app     = express.createServer();
+
+// Use a configure block to tell Express how to act, generally
+app.configure(function(){
+  // Dunno what these do
+  app.use(express.methodOverride());
+  app.use(express.bodyDecoder());
+  app.use(app.router);
+  // Statically serve anything in /public
+  app.use(express.staticProvider(__dirname + '/public'));
+  // Be verbose about errors, will move this into a specific environment later
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-app.listen(80);
+
+// Screw Jade for now, will figure it out later
+/*app.register('.html', require('jade'));
+app.set('view engine', 'jade');
+*/
+
+// Auto-redirect the root to the static index.html file
+app.get('/', function(req, res){
+  res.redirect('index.html');
+});
+
+// Listen on 3000
+app.listen(3000);
+console.log('Server running at localhost on port 3000')
