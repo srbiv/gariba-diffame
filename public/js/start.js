0fd995008a069704d385ec67367d2b6ddf102660
Stafford Brooke <stafford@inquus.com>
2010-08-29 00:38:33 -0400

    logo

--- public/js/start.js
+++ public/js/start.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('.user-list ul').addClass('ui-widget-content');
-  $('.user-list h2').addClass('ui-widget-header');
+  $('.user-list h2').addClass('ui-widget-header ui-corner-top');
   $('#state a').button();
 });
