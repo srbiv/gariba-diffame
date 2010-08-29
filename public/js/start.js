60f5e9ecd778430596a59791a91c79e71a55ee8d
Stafford Brooke <stafford@inquus.com>
2010-08-29 17:56:49 -0400

    rules

--- public/js/start.js
+++ public/js/start.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
   $('.user-list ul').addClass('ui-widget-content');
   $('.user-list h2').addClass('ui-widget-header ui-corner-top');
   $('#state a').button();
+  $('.back').button();
 });
